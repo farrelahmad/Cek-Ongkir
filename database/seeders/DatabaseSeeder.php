bec84039458382beae3b69a25a5a9ed090a6064e
FarrelAhmad <farrel.aurell@gmail.com>
2023-04-07 10:24:29 +0700

    add some data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call(CitySeeder::class);
+        $this->call(CourierSeeder::class);
+        $this->call(ProvinceSeeder::class);
     }
 }
