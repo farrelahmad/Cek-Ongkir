41fb349c6f6c31d40ff31b2372975b3f0b28cf82
FarrelAhmad <farrel.aurell@gmail.com>
2023-04-07 10:03:12 +0700

    add json

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,13 @@ class CitySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $filecity = file_get_contents(base_path('/database/city.json'));
+        $fileregency = file_get_contents(base_path('/database/regency.json'));
+
+        $datacity = json_decode($filecity, true);
+        $dataregency = json_decode($fileregency, true);
+
+        City::insert($datacity);
+        City::insert($dataregency);
     }
 }
